86ed97da5eb9e39af1f328207c96e1691ecbc400
franout <s262620@studenti.polito.it>
2020-02-16 17:44:50 +0100

    compiling nvdla sw stack on the board

--- build_software.sh
+++ build_software.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-## deprecated
-## TODO 
-# nvdla software
-## build linux kernel mode driver
-cd nvdla/sw/kmd
-make KDIR=./firmware ARCH=arm CROSS_COMPILE=/usr/bin/arm-none-eabi-gcc-4.9.3
+#############################################################
+#####RUN ON THE BOARD WITH PROTOBUFF ALREADY INSTALLED ######
+#############################################################
 
-cd ../umd
+
+### paths are relative to the board
+
+### build the KMD 
+cd ./nvdla/sw/kmd
+make KDIR="$HOME"/moduled/nv_small/build/tmp/work-shared/plnx-zynq7/kernel-source/ ARCH=arm CROSS_COMPILE="$HOME"/petalinux/tools/xsct/gnu/aarch64/lin/aarch64-none/bin/aarch64-none-elf-
+exit
+
+cd ../
+exit
 ## build user mode driver
-export TOP=./
+cd ./nvdla/sw/umd
+
+export LD_LIBRARY_PATH=/usr/local/bin
+export TOP=$PWD
+## petalinux compiler arm cross compiler  
+export TOOLCHAIN_PREFIX=/usr/bin/arm-linux-gnueabihf-
 make compiler 
 make runtime
--- build_zynq/build_and_deploy_distro.sh
+++ build_zynq/build_and_deploy_distro.sh
@@ -4,6 +4,7 @@ core="/media/fra/DATA/uni/2019-2020/thesis/cogitantium/"
 PETALINUX_PATH="$HOME"
 # SoC name 
 export MACHINE="zc702-zynq7"
+export ARCH="arm"
 source "$PETALINUX_PATH"/petalinux/settings.sh
 
 
@@ -13,63 +14,62 @@ petalinux-create -t project   --template zynq --name nv_small
 
 cd  ./nv_small
 ## copy configuration with external local path for u-boot and linux kernel and DTB ( generated from Vitis) 
-cp ../config ./project-spec/configs/config
+#cp ../config ./project-spec/configs/config
 
 ## copy configuration from rootfs for adding libc ( otherwise nvdla appl will complain )
-cp ../rootfs_config ./project-spec/configs/rootfs_config
+#cp ../rootfs_config ./project-spec/configs/rootfs_config
 
 
 ## get hardware description generated from VIVADO
 petalinux-config --get-hw-description="$core"nvdla_configurations/nv_small_512_32_8_int8/
 
 ## KMD nvdla
-petalinux-create -t modules --name kmdnvdla --enable
-rm -rf ./project-spec/meta-user/recipes-module/kmdnvdla/files/*
-cp -r "$core"nvdla/sw/kmd/* ./project-spec/meta-user/recipes-modules/kmdnvdla/files/
+#petalinux-create -t modules --name kmdnvdla --enable
+#rm -rf ./project-spec/meta-user/recipes-module/kmdnvdla/files/*
+#cp -r "$core"nvdla/sw/kmd/* ./project-spec/meta-user/recipes-modules/kmdnvdla/files/
 
-## putting in package lib 
-echo  "FILES_\${PN} += "${libdir}/*" FILES_\${PN}-dev = "${libdir}/* ${includedir}"" >> ./project-spec/meta-user/recipes-modules/kmdnvdla/kmdnvdla.bb
+#echo "inherit module" >> ./project-spec/meta-user/recipes-modules/kmdnvdla/kmdnvdla.bb
+#EXTRA_OEMAKE = 'KERNEL_src="${STAGING_KERNEL_DIR}" \
+#                O=${STAGING_KERNEL_BUILDDIR} \" 
 
-## create user application UMDnvdla
+
+## putting in package lib
+#echo "FILES_\${PN} += \"\${base_libdir}/modules/\""  >> ./project-spec/meta-user/recipes-modules/kmdnvdla/kmdnvdla.bb
+#echo "FILES_\${PN} += libs/ " >> ./project-spec/meta-user/recipes-modules/kmdnvdla/kmdnvdla.bb
+
+## create api for  UMDnvdla ( portability layer for linux ) 
 #petalinux-create -t apps --name umdnvdlaapi --enable
 #rm -rf ./project-spec/meta-user/recipes-apps/umdnvdlaapi/files/*
 #copy only the portability layer
-#cp -r "$core"nvdla/sw/umd/port/* ./project-spec/meta-user/recipes-apps/umdnvdlaapi/files/
-
+#cp -r "$core"nvdla/sw/umd/port/linux/* ./project-spec/meta-user/recipes-apps/umdnvdlaapi/files/
+#mv ./project-spec/meta-user/recipes-apps/umdnvdlaapi/files/nvdla.c ./project-spec/meta-user/recipes-apps/umdnvdlaapi/files/umdnvdlaapi.c
 ## put the module and application in the rootfs nd kernel  modules--> 
 petalinux-config -c rootfs
 petalinux-config -c kernel
 
-
 ## rebuild systems DO NOT CHANGE ORDER
-petalinux-build -c kernel
-petalinux-build -c kmdnvdla
+#petalinux-build -c kernel
+#petalinux-build -c kmdnvdla
+#petalinux-build -c kmdnvdla -x do_install
+
 #petalinux-build -c umdnvdla
-petalinux-build -c rootfs
+#petalinux-build -c rootfs
 ## install umdnvdla
 #petalinux-build -c umdnvdla -x do_install
 
 # build and add nvdla module and application to the rootfs and into the kernel
 petalinux-build
 
-exit
 ## generate the boot image ( boot.bin )  
 ## fsbl generated from vitis, configuration bitstream from vivado
-petalinux-package --boot --fsbl "$core"nvdla_configurations/nv_small_512_32_8_int8/FSBL.elf --fpga "$core"nvdla_configurations/nv_small_512_32_8_int8/base_zynq_wrapper.bit --u-boot
-
-## package bsp
-petalinux-package --bsp -p ./  --hwsource "$core"/nvdla/project_3/ --output nv_small.BSP
-
-## package sysroot
-petalinux-package --sysroot
-
+petalinux-package --boot --fsbl "$core"nvdla_configurations/nv_small_512_32_8_int8/fsbl.elf --fpga "$core"nvdla_configurations/nv_small_512_32_8_int8/base_zynq_wrapper.bit --u-boot
 #######################################
 ###### boot board/prepare sd ##########
 #######################################
-
+#petalinux-package --image
 
 ## through jtag
-if [ boot_device -e "jtag" ] then 
+if [ $boot_device -e "jtag" ] then 
 	##  Downloads the prebuilt FPGA bitstream and FSBL, boots theprebuilt U-Boot, and boots the prebuilt kernel on targe
 #1. Power off the board.
 #2. Connect the JTAG port on the board with the JTAG cable to your workstation.
@@ -81,15 +81,19 @@ if [ boot_device -e "jtag" ] then
 #(For example, kermit, minicom) with the baud rate set to 115200 on that console.
 #8. Run the petalinux-boot command as follows on your workstation:$ petalinux-boot --jtag --prebuilt 3 
 #9. Type user name root and password root on the serial console to log into the PetaLinuxsystem.
+#7. Open a console on your workstation and start with preferred serial communication program
+#(For example, kermit, minicom) with the baud rate set to 115200 on that console.
+#8. Run the petalinux-boot command as follows on your workstation:$ petalinux-boot --jtag --prebuilt 3 
+#9. Type user name root and password root on the serial console to log into the PetaLinuxsystem.
 #10.Determine the IP address of the PetaLinux by running ifconfig on the system console.
 
 gtkterm
 echo "set serial communication with baoud rate 115200"
 echo "WAIT FOR SETTING UP THE SERIAL COMMUNICATION -> PROGRAMMING  THE BOARD"
 read stdin
-
+petalinux-package --prebuilt --fpga "$core"/nvdla_configurations/nv_small_512_32_8_int8/base_zynq_wrapper.bit
 petalinux-boot --jtag --prebuilt 3
-elif [ boot_device -e "sd"  ] then
+elif [ $boot_device -e "sd"  ] then
 ## through sd
 #1. Mount the SD card on your host machine.
 #2. Copy the following files from <plnx-proj-root>/pre-built/linux/images/ into the
@@ -106,8 +110,11 @@ elif [ boot_device -e "sd"  ] then
 
 
 ## check for sd card
-cp ./pre-built/linux/images/BOOT.BIN  sd dest
-cp ./pre-built/linux/images/image.ub
+ cp images/linux/BOOT.BIN /media/fra/BOOT/
+ cp images/linux/image.ub /media/fra/BOOT/
+
+sudo tar xvf ./images/linux/rootfs.tar.gz -C /media/fra/rootfs
+
 #unmount the sd card
 
 echo "remove  THE SD CARD"
@@ -115,7 +122,15 @@ echo "remove  THE SD CARD"
 gtkterm
 echo "set serial communication with baoud rate 115200"
 echo "WAIT FOR SETTING UP THE SERIAL COMMUNICATION -> POWER ON THE BOARD WITH SD CARD MOUNTED AND JUMPER SETTED"
-read stdin
+
 
 fi
 
+#1. The SD card is formatted with two partitions using a partition editor such as gparted.
+#2. The first partition should be at least 500 MB in size and formatted as a FAT32 file system.
+#Ensure that there is 4 MB of free space preceding the partition. The first partition will contain
+#the boot loader, device tree, and kernel images. Label this partition as BOOT.
+#3. The second partition should be formatted as an ext4 files system and can take up the
+#remaining space on the SD card. This partition will store the system root file system. Label
+#this partition as RootFS.
+#For optimal performance ensure that the SD card partitions are 4 MB aligned.
